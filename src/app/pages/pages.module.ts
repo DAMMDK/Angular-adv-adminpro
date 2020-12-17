81836a5046920b3928c72f8907218bc2090715ce
DAMMDK <54679014+DAMMDK@users.noreply.github.com>
2020-12-17 11:22:41 -0600

    Fin sección 6

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+// Modulos
+import { ComponentsModule } from '../components/components.module';
+import { SharedModule } from '../shared/shared.module';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
-import { SharedModule } from '../shared/shared.module';
 import { AuthModule } from '../auth/auth.module';
 
 
@@ -28,7 +31,9 @@ import { AuthModule } from '../auth/auth.module';
     CommonModule,
     SharedModule,
     AuthModule,
-    RouterModule
+    RouterModule,
+    FormsModule,
+    ComponentsModule
   ]
 
 })
