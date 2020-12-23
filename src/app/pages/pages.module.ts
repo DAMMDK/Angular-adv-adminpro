72267ec669865093e5f5fed52a30119674e4bcb1
DAMMDK <54679014+DAMMDK@users.noreply.github.com>
2020-12-23 09:32:58 -0600

    Fin sección 7

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -12,6 +12,7 @@ import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
 import { AuthModule } from '../auth/auth.module';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 
 @NgModule({
@@ -19,13 +20,15 @@ import { AuthModule } from '../auth/auth.module';
     DashboardComponent,
     ProgressComponent,
     Grafica1Component,
-    PagesComponent
+    PagesComponent,
+    AccountSettingsComponent
   ],
   exports: [
     DashboardComponent,
     ProgressComponent,
     Grafica1Component,
-    PagesComponent
+    PagesComponent,
+    AccountSettingsComponent
   ],
   imports: [ 
     CommonModule,
